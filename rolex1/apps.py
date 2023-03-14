efb24d66101c059772815b093aea1cc32c301ce1
dhiili <narayanavaramdhilli7461.com>
2023-03-14 20:54:00 +0530

    first commit

---
+++ rolex1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Rolex1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rolex1'
---
+++ rolex1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def dilli1(request):
+    return HttpResponse('<h1> i like music ver much,/h1>')
+
+# Create your views here.
